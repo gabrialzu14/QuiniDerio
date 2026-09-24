dfea844d2a3f8ec21a2e6d7365909f8503659fbe
gabrialzu14 <gabrialzueta@gmail.com>
2026-09-24 14:54:15 +0200

    Initial QuiniDerio app foundation

---
+++ app/layout.tsx
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"QuiniDerio",description:"La quiniela de los equipos del CD Derio"};
+export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="es"><body>{children}</body></html>}
